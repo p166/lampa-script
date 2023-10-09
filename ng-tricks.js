f6d98ddc2c167c18529b111a13c5a375a14591aa
p166 <dsimakov80@gmail.com>
2023-10-09 16:00:22 +0400

    remove Metrics

--- ng-tricks.js
+++ ng-tricks.js
@@ -736,28 +736,6 @@ if(Lampa.Storage.field('SISI_fix') == true) $("[data-action=sisi]").eq(0).show()
 			}
 		});
 /*End ТоррСервер */
-/* METRIKA */
-			(function(m, e, t, r, i, k, a) {
-				m[i] = m[i] || function() {
-					(m[i].a = m[i].a || []).push(arguments)
-				};
-				m[i].l = 1 * new Date();
-				for(var j = 0; j < document.scripts.length; j++) {
-					if(document.scripts[j].src === r) {
-						return;
-					}
-				}
-				k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-			})
-			(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-			ym(92135047, "init", {
-				clickmap: true,
-				trackLinks: true,
-				accurateTrackBounce: true
-			});
-			var METRIKA = '<noscript><div><img src="https://mc.yandex.ru/watch/92135047" style="position:absolute; left:-9999px;" alt="" /></div></noscript>';
-			$('body').append(METRIKA);
-/* End METRIKA */		
 
 /*
 * ШАБЛОНЫ
